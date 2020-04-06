2f6d21afc44ae72651dcbf2d41cee3b96260b449
chenshiyu <chenshiyu@ygsoft.com>
2020-04-06 15:24:02 +0800

    feat:新增不输入指令默认为ls

--- src/cli.js
+++ src/cli.js
@@ -5,27 +5,32 @@ const config = require('../package.json');
 const api = require('./index');
 const version = config.version;
 
-  program.version(version).name('t');
+program.version(version).name('t');
+
+const setCommand = () => {
+  program
+    .command('ls')
+    .option('[todo|done], list todo/done/add lists')
+    .description('list tasks and handle it')
+    .action((env) => {
+      api.handleList(env.args[0]);
+    });
 
   program
-  .command('ls')
-  .option('[todo|done], list todo/done/add lists')
-  .description('list tasks and handle it')
-  .action((env) => {
-    api.handleList(env.args[0]);
-  });
+    .command('add <task>')
+    .description('add (a) task(s)')
+    .action(() => api.add(process.argv));
 
   program
-  .command('add <task>')
-  .description('add (a) task(s)')
-  .action(() => api.add(process.argv));
+    .command('clear')
+    .option('[todo|done], clear todo/done/add lists')
+    .description('clear tasks')
+    .action((env) => {
+      api.clear(env.args[0]);
+    });
+};
 
-  program
-  .command('clear')
-  .option('[todo|done], clear todo/done/add lists')
-  .description('clear tasks')
-  .action((env) => {
-    api.clear(env.args[0]);
-  });
+// 如果这里不做判断，不输入参数的时候list出不来
+if (process.argv.length === 2) { api.handleList(); } else { setCommand(); }
 
-  program.parse(process.argv);
+program.parse(process.argv);
